eab247822f7d2b1a40066a49548ddedd8e81360d
mfloto <60036186+mfloto@users.noreply.github.com>
2023-01-19 10:43:15 +0100

    WIP subscriber

--- src/tcp_handling.rs
+++ src/tcp_handling.rs
@@ -18,7 +18,7 @@ use tokio::io::AsyncReadExt;
 use tokio::net::TcpStream;
 use tokio::sync::broadcast::Receiver;
 use tokio::sync::mpsc::Sender;
-use tracing::info;
+use tracing::{debug, info};
 
 #[derive(Debug)]
 pub struct Client {
@@ -41,7 +41,7 @@ impl Client {
         loop {
             match stream.readable().await {
                 Ok(_) => match self.handle_packet(&mut stream).await {
-                    Ok(_) => continue,
+                    Ok(_) => {}
                     Err(_) => {
                         info!("Closing client {0}", &addr);
                         break;
@@ -50,15 +50,20 @@ impl Client {
                 Err(ref e) => info!("ERROR {:?} connection: {:?}", addr, e),
             }
             if let Some(receiver) = &mut self.receiver {
-                match receiver.recv().await {
-                    Ok(Message::Publish(packet)) => {
-                        let send_packet = Packet::Publish(packet);
-                        Self::write_to_stream(&mut stream, &send_packet).await
-                    }
-                    Ok(Message::Subscribe(m)) => continue,
-                    Ok(Message::Unsubscribe(m)) => continue,
-                    Err(_) => continue,
-                };
+                debug!("Client has receiver!");
+                if !receiver.is_empty() {
+                    debug!("Receiver has messages");
+                    match receiver.recv().await {
+                        Ok(Message::Publish(packet)) => {
+                            info!("Subscriber received new message");
+                            let send_packet = Packet::Publish(packet);
+                            Self::write_to_stream(&mut stream, &send_packet).await
+                        }
+                        Ok(Message::Subscribe(m)) => continue,
+                        Ok(Message::Unsubscribe(m)) => continue,
+                        Err(_) => continue,
+                    };
+                }
             }
         }
     }
@@ -74,6 +79,7 @@ impl Client {
     #[tracing::instrument]
     #[async_backtrace::framed]
     async fn handle_publish_packet(
+        &mut self,
         stream: &mut TcpStream,
         peer: &SocketAddr,
         packet: &PublishPacket,
@@ -85,6 +91,8 @@ impl Client {
         );
         // Packet with a QoS of 0 do get a PUBACK
         if packet.qos != QoS::AtMostOnce {
+            self.topics.lock().unwrap().publish(packet.clone());
+
             let puback = Packet::PublishAck(PublishAckPacket {
                 packet_id: packet.packet_id.unwrap(),
                 reason_code: mqtt_v5::types::PublishAckReason::Success,
@@ -202,9 +210,7 @@ impl Client {
                         Self::handle_connect_packet(stream, &peer, &p).await
                     }
                     Some(Packet::PingRequest) => Self::handle_pingreq_packet(stream).await,
-                    Some(Packet::Publish(p)) => {
-                        Self::handle_publish_packet(stream, &peer, &p).await
-                    }
+                    Some(Packet::Publish(p)) => self.handle_publish_packet(stream, &peer, &p).await,
                     Some(Packet::Subscribe(p)) => {
                         self.handle_subscribe_packet(stream, &peer, &p).await
                     }
--- src/topics.rs
+++ src/topics.rs
@@ -31,6 +31,19 @@ impl Topics {
             Ok(self.add(name)?)
         }
     }
+    /// Publishes [packet] to the topic specified in [packet]
+    pub(crate) fn publish(&mut self, packet: PublishPacket) -> Result {
+        let topic_name = packet.topic.topic_name().to_string();
+        if let Some(channel) = self.0.get_mut(&topic_name) {
+            channel.sender.send(Message::Publish(packet)).unwrap();
+        } else {
+            info!("Topic {:?} does not exist... Creating it", topic_name);
+            let _ = self.add(Cow::Owned(topic_name.clone()));
+            let channel = self.0.get_mut(&topic_name).unwrap();
+            channel.sender.send(Message::Publish(packet)).unwrap();
+        }
+        Ok(())
+    }
 }
 #[derive(Clone, Debug, PartialEq)]
 pub enum Message {
